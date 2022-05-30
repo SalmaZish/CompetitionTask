713669a56b77b330fd00325953344181ee344d18
Aftab <aftab.khateeb@energyq.com.au>
2022-05-31 00:25:45 +1000

    ShareSkill Commit

---
+++ Tests/ShareSkill_Tests.cs
@@ -0,0 +1,78 @@
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using System.IO;
+using System.Data;
+using System.Diagnostics;
+using Competition_task_2.Utilities;
+using Competition_task_2.Pages;
+
+namespace CompetitionTasks.Pages
+{
+    [TestFixture]
+
+    public class ShareSkillDetails_Tests : CommonDriver
+    {
+        [Test, Order(1)]
+        public void ExcelReaderMethod()
+        {
+            try
+            {
+                ExcelOperations.ClearData();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        [Test, Order(2)]
+        public void CreateShareSkillDetails_Test()
+        {
+            ExcelOperations.ReadDataTable(stream, "ShareSkill");
+            test = extent.CreateTest("test1").Info("Test started");
+
+            //Profile create page object initialization and definition
+            ShareSkillPage shareSkillPageObj = new ShareSkillPage();
+            shareSkillPageObj.ShareSkillSteps(driver);
+            test.Log(Status.Info, "shareskill details saved");
+            test.Log(Status.Pass, "Test passed");
+
+
+
+        }
+        [Test, Order(3)]
+        public void EditSkillDetails_Test()
+        {
+            ExcelOperations.ReadDataTable(stream, "ManageListing");
+            test = extent.CreateTest("test2").Info("Test started");
+            //edit
+            ManageListingPage manageListingPageObj = new ManageListingPage();
+
+            manageListingPageObj.EditingSteps(driver);
+            test.Log(Status.Info, "shareskill details edited");
+            test.Log(Status.Pass, "Test passed");
+
+            // Assert.That(editCheck.Text == "Service Type", " not created successfully");
+
+        }
+        [Test, Order(4)]
+        public void DeleteSkillDetails_Tests()
+        {
+            test = extent.CreateTest("test3").Info("Test started");
+            ManageListingPage manageListingPageObj = new ManageListingPage();
+            manageListingPageObj.deleteSteps(driver);
+            test.Log(Status.Info, "shareskill details deleted");
+            test.Log(Status.Pass, "Test passed");
+            //takeScreenshot(driver);
+            // IWebElement editCheck = driver.FindElement(By.XPath("//*[@id='listing-management-section']/div[2]/div[1]/div[1]/table/thead/tr/th[5]"));
+            // Assert.That(editCheck.Text == "Service Type", " not created successfully");
+        }
+
+
+    }
+}
+
---
+++ Utilities/CommonDriver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using ExcelDataReader;
+using Competition_task_2.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Data;
+
+namespace Competition_task_2.Utilities
+{
+    public class CommonDriver
+    {
+        public static IWebDriver driver;
+        public static ExtentReports extent;
+        public static ExtentHtmlReporter htmlreport;
+        public static ExtentTest test;
+        public static string ScreenshotFileName;
+        public static FileStream stream;
+
+
+        [OneTimeSetUp]
+
+        public void LoginFunction()
+
+        {
+            string fileName = @"T:\Salma_Testing\IndustryConnect\Internship\Competition_Task\Competition task 2\InputFile\ExcelOperationsDetails.xlsx";
+            //open file and returns as stream
+            stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            ExcelOperations.ReadDataTable(stream, "LoginSheet");
+
+            //extent report
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"T:\Salma_Testing\IndustryConnect\Internship\Competition_Task\Competition task 2\InputFile\CommonDriver.html");
+            extent.AttachReporter(htmlReporter);
+
+            //open chrome
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            //Login page object initialization and definition
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginPages(driver);
+        }
+
+        [OneTimeTearDown]
+        public void CloseTestRun()
+        {
+            driver.Close();
+            extent.Flush();
+        }
+    }
+}
